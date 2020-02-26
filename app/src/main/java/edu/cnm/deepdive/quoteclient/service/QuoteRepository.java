1aef2e8ff5e019f569430b548c1c9a37da3a9c72
Your Name Comes Here <archie808@hotmail.com>
2020-02-26 16:59:28 -0700

    Created QodService and Quote class.

---
+++ app/src/main/java/edu/cnm/deepdive/quoteclient/service/QuoteRepository.java
@@ -0,0 +1,34 @@
+package edu.cnm.deepdive.quoteclient.service;
+
+import edu.cnm.deepdive.quoteclient.model.Quote;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class QuoteRepository {
+
+  private static final int NETWORK_POOL_SIZE = 10;
+
+  private final QodService proxy;
+  private final Executor newtworkPool;
+
+  private QuoteRepository() {
+    proxy = QodService.getInstance();
+    newtworkPool = Executors.newFixedThreadPool(NETWORK_POOL_SIZE);
+  }
+
+  public static QuoteRepository getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  public Single<Quote> getRandom() {
+    return proxy.getRandom()
+        .subscribeOn(Schedulers.from(newtworkPool));
+  }
+  private static class InstanceHolder {
+
+    private static final QuoteRepository INSTANCE = new QuoteRepository();
+  }
+
+}
